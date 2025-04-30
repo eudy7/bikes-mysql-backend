054bbd3605f38899b1f730c04c1cdcd5c8641af6
eudy404 <eudydeluna95@gmail.com>
2025-04-30 14:12:10 -0600

    Fix definitivo: app bien importado en index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
-
-const server = require('./server');
+const app = require('./server');
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(8081, () => {
- console.log('🚴 Servidor de bicicletas escuchando en http://localhost:8081');
+app.listen(PORT, () => {
+  console.log(`🚴 Servidor de bicicletas escuchando en http://localhost:${PORT}`);
 });
