80fe79b761d11d357582c76d9cd5a85da1e1aecc
eudy404 <eudydeluna95@gmail.com>
2025-05-01 18:29:10 -0600

    Guardar cambios de soporte para campo 'image' en modelo, controlador y migración

--- controllers/bikeController.js
+++ controllers/bikeController.js
@@ -13,9 +13,11 @@ const getBikes = async (req, res) => {
 
 // Agregar una nueva bicicleta
 const addBike = async (req, res) => {
-  const { brand, model, price, description } = req.body;
   try {
-    const newBike = await Bike.create({ brand, model, price, description });
+    const { brand, model, price, description } = req.body;
+    const image = req.file ? req.file.filename : null;
+
+    const newBike = await Bike.create({ brand, model, price, description, image });
     res.status(201).json(newBike);
   } catch (error) {
     res.status(500).json({ error: 'Error al agregar bicicleta' });
--- models/bike.js
+++ models/bike.js
@@ -1,26 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Bike extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
+
   Bike.init({
     brand: DataTypes.STRING,
     model: DataTypes.STRING,
     price: DataTypes.FLOAT,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Bike',
   });
+
   return Bike;
 };
