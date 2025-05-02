ec3581b5ecc2377db6a65b3ac4eaaf74bc9a9793
eudy404 <eudydeluna95@gmail.com>
2025-05-01 19:42:52 -0600

    Agrega rutas PUT y DELETE para CRUD completo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const bikeController = require('../controllers/bikeController');
 
+// GET - Obtener todas las bicicletas
 router.get('/bikes', bikeController.getBikes);
+
+// POST - Agregar una bicicleta
 router.post('/bikes', bikeController.addBike);
+
+// PUT - Actualizar una bicicleta
+router.put('/bikes/:id', bikeController.updateBike);
+
+// DELETE - Eliminar una bicicleta
+router.delete('/bikes/:id', bikeController.deleteBike);
 
 module.exports = router;
