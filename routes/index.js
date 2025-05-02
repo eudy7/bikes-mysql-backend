58ba969af03687892eacf0587fef7edcd042dc4e
eudy404 <eudydeluna95@gmail.com>
2025-05-02 00:49:43 -0600

    Fix: agregar multer a POST /bikes para subida de imagen

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bikeController = require('../controllers/bikeController');
+const multer = require('multer');
 
-// GET - Obtener todas las bicicletas
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage });
+
 router.get('/bikes', bikeController.getBikes);
 
-// POST - Agregar una bicicleta
-router.post('/bikes', bikeController.addBike);
+router.post('/bikes', upload.single('image'), bikeController.addBike);
 
-// PUT - Actualizar una bicicleta
 router.put('/bikes/:id', bikeController.updateBike);
 
-// DELETE - Eliminar una bicicleta
 router.delete('/bikes/:id', bikeController.deleteBike);
 
 module.exports = router;
